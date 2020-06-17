e1aa16c36c31ef200bb1eb6ccaa5098461a5b239
katanaevav <katanaev@gmail.com>
2020-06-18 01:03:52 +0400

    Созданы начальные моковые данные для 8 фильмов

---
+++ src/mocks/films.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: `The Shawshank Redemption`,
+    genre: `drama`,
+    year: 1994,
+  },
+  {
+    title: `The Green Mile`,
+    genre: `drama`,
+    year: 1999,
+  },
+  {
+    title: `Forrest Gump`,
+    genre: `melodrama`,
+    year: 1994,
+  },
+  {
+    title: `Schindler's List`,
+    genre: `drama`,
+    year: 1993,
+  },
+  {
+    title: `Intouchables`,
+    genre: `comedy`,
+    year: 2001,
+  },
+  {
+    title: `Inception`,
+    genre: `fantasy`,
+    year: 2010,
+  },
+  {
+    title: `Leon`,
+    genre: `action`,
+    year: 1994,
+  },
+  {
+    title: `The Lion King`,
+    genre: `adventure`,
+    year: 1994,
+  }
+];
