5125ab1265b0cb8819fab8a5095f38c239f7d2de
KatanaevAV <katanaev@gmail.com>
2020-07-14 12:30:06 +0400

    В плеере длительность видео исправлена на оставшееся время

--- src/components/video-progress/video-progress.jsx
+++ src/components/video-progress/video-progress.jsx
@@ -1,6 +1,11 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 
+const SECONDS_IN_HOUR = 3600;
+const MINUTES_IN_HOUR = 60;
+const SECONDS_IN_MINUTE = 60;
+const MAX_DURATION_IN_PERCENT = 100;
+
 class VideoProgress extends PureComponent {
   constructor(props) {
     super(props);
@@ -8,17 +13,19 @@ class VideoProgress extends PureComponent {
   }
 
   _getDurationTime() {
-    const {duration} = this.props;
-    const hours = Math.floor(duration / 3600);
-    const minutes = Math.floor(duration / 60) - (hours * 60);
-    const seconds = duration - (minutes * 60);
+    const {currentProgress, duration} = this.props;
+    const timeLeft = duration - currentProgress;
+
+    const hours = Math.floor(timeLeft / SECONDS_IN_HOUR);
+    const minutes = Math.floor(timeLeft / SECONDS_IN_MINUTE) - (hours * MINUTES_IN_HOUR);
+    const seconds = timeLeft - (minutes * SECONDS_IN_MINUTE);
     return `${hours > 0 ? `${hours}:` : ``}${minutes > 0 ? `${minutes}:` : ``}${seconds > 0 ? `${seconds}` : ``}`;
   }
 
   _getCurrentProgress() {
     const {currentProgress, duration} = this.props;
 
-    return duration === 0 ? 0 : Math.floor(100 * currentProgress / duration);
+    return duration === 0 ? 0 : Math.floor(MAX_DURATION_IN_PERCENT * currentProgress / duration);
   }
 
   render() {
