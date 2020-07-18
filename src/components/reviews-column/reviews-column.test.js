ba01cb96af36bd4da4d8f724766c0ccea0854415
katanaevav <katanaev@gmail.com>
2020-07-19 01:51:32 +0400

    Обновлены тесты

--- src/components/reviews-column/reviews-column.test.js
+++ src/components/reviews-column/reviews-column.test.js
@@ -8,21 +8,21 @@ const Reviews = [
     text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
     author: `Kate Muir`,
     date: Date.parse(`June 3, 2019`),
-    mark: `5`,
+    mark: 5,
   },
   {
     id: 1,
     text: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
     author: `Paula Fleri-Soler`,
     date: Date.parse(`December 11, 2019`),
-    mark: `5`,
+    mark: 5,
   },
   {
     id: 2,
     text: `I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.`,
     author: `Amanda Greever`,
     date: Date.parse(`November 6, 2019`),
-    mark: `4`,
+    mark: 4,
   },
 ];
 
